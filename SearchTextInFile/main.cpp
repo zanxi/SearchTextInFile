2bdae1dee720611f981384f07d70b2135f5d4b24
zanxi <consultantweb25@gmail.com>
2022-11-02 08:09:22 +0300

    правки чтения

--- SearchTextInFile/main.cpp
+++ SearchTextInFile/main.cpp
@@ -20,6 +20,7 @@ void OutPut(string fname, string mask, map_InfoSearchStroki safe_info)
 {
 	ofstream fout("output.txt");
 	fout << "file: " << fname << " | mask: " << mask << '\n' << endl;
+	fout << safe_info->size() << '\n' << endl;
 	for (std::map<int, InfoSearchStroka>::iterator it = safe_info->begin(); it != safe_info->end(); ++it)
 	{
 		//std::cout << it->first << " => " << it->second. << '\n';
